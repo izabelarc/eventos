b583e54f600c784b58770fb037e048aad67120f5
izabelarc <136197005+izabelarc@users.noreply.github.com>
2023-09-17 20:02:54 -0300

    Add files via upload

---
+++ jumpscare.py
@@ -0,0 +1,14 @@
+'''
+ pip install --upgrade setuptools wheel
+ pip install playsound
+ https://drive.google.com/drive/folders/1He2bQlOXXhHqZ_3hhQdtCj_ts1-X5kzb?usp=sharing
+ pip install opencv-python
+ https://dontpad.com/terror_infinity
+'''
+import sys
+import cv2
+import playsound
+
+playsound.playsound("./fuzzy-jumpscare.mp3")
+
+window_name = "BUU"
