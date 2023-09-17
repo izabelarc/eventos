b7977aa6060e70bf9954bae15670ad64aa158cf9
izabelarc <136197005+izabelarc@users.noreply.github.com>
2023-09-17 17:58:00 -0300

    Add files via upload

---
+++ freira.py
@@ -0,0 +1,17 @@
+# print("Jornada de terror!")
+# print("Cuidado com a freira!")
+
+# hora = input("Digite um horário: ")
+# print(hora)
+# print(f"{hora}")
+
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+print(f"A soma do número {numero1} com o número {numero2} é {soma}")
+print(f"A subtração do número {numero1} pelo número {numero2} é {subtracao}")
+print(f"A multiplicação do número {numero1} pelo número {numero2} é {multiplicacao}")
+print(f"A divisão do número {numero1} pelo número {numero2} é {divisao}")
